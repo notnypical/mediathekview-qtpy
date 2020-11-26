e9df62a24720f6d4d584a06f3675f93c5d7baec5
notnypical <46350569+notnypical@users.noreply.github.com>
2020-11-26 23:49:58 +0100

    Add basic application

---
+++ main.py
@@ -0,0 +1,35 @@
+# This Python file uses the following encoding: utf-8
+#
+# Copyright 2020 NotNypical, <https://notnypical.github.io>.
+#
+# This file is part of MediathekView-QtPy.
+#
+# MediathekView-QtPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MediathekView-QtPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MediathekView-QtPy.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import sys
+
+from PySide2.QtWidgets import QApplication
+
+from main_window import MainWindow
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
